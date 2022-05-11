a7d3a96f328ea71883755d6415b4a49640fb38a6
swaags <105006813+swaags@users.noreply.github.com>
2022-05-11 11:10:52 -0400

    Add files via upload

---
+++ Baseline4Plot.m
@@ -0,0 +1,72 @@
+
+%plot things from each half cycle (can just copy and paste this section
+%once this has been run once)
+sz = [length(chargeCycles) 8];
+varNames = {'cycle_number','max_stress','start_stress','stress_change','capacity_start_mAhg','capacity_end_mAhg','charge_current','c_rate'};
+chronoArray = zeros(sz);
+for i = 1:length(chargeCycles)
+    hsi = find(chargeCycles{i}(:,6)<.011);
+    maxStrs = chargeCycles{i}(hsi(1),2);
+    startStrs = chargeCycles{i}(1,2);
+    strsChg = maxStrs-startStrs;
+    crate = round(abs(chargeCycles{i}(2,7)/C_rate),2);
+    chronoArray(i,:) = [i, maxStrs, startStrs, strsChg, chargeCycles{i}(hsi(1),9)/mass, chargeCycles{i}(end,9)/mass,chargeCycles{i}(2,7),crate];
+end
+
+chrono = array2table(chronoArray,'VariableNames',varNames)
+%writetable(chrono,fullfile(projdir,'Baseline4Chronology.csv'))
+
+
+figure(6)
+yyaxis left
+plot(chrono.cycle_number,chrono.start_stress,'-bo')
+hold on
+plot(chrono.cycle_number,chrono.max_stress,'-bs')
+plot(chrono.cycle_number,chrono.stress_change,'-b*')
+ylabel('Stress MPa')
+yyaxis right
+plot(chrono.cycle_number,chrono.capacity_start_mAhg,'-o')
+plot(chrono.cycle_number,chrono.capacity_end_mAhg,'-s')
+hold off
+legend('Stress at start of charge','Stress at end of charge','Stress Change','Capacity at hold start','Capacity at hold end')
+xlabel('Cycle Number')
+ylabel('Capacity mAh/g')
+title('Baseline 4 Cycles')
+
+%{
+figure(3)
+hold on
+for i = cycIndex'
+    if halfCycles{i}.ox_red(2) == 0
+        plot(halfCycles{i}.QDischarge_mA_h,halfCycles{i}.stress_MPa)
+        %EC labs definition of charge is the opposite of ours
+        title('Baseline 4')
+        xlabel('Charge mAh')
+        ylabel('Stress MPa')
+    end
+end
+
+figure(4)
+hold on
+for j = cycIndex'
+    if halfCycles{j}.ox_red(2) == 1
+        plot(halfCycles{j}.QCharge_mA_h,halfCycles{j}.stress_MPa)
+        %EC labs definition of charge is the opposite of ours
+        title('Baseline 4')
+        xlabel('Discharge mAh')
+        ylabel('Stress MPa')
+    end
+end
+
+figure(1)
+plot(B4master.time_s/3600,B4master.stress_MPa);
+set(gca,'FontSize',26)
+xlabel('Time (Hrs)'); ylabel('Stress (MPa)');
+hold on;
+plot(B4master.time_s/3600,B4master.x_I__mA)
+plot(B4master.time_s/3600,B4master.Ewe_V)
+hold off;
+
+figure(2)
+plot(B4master.stress_MPa,B4master.Ewe_V)
+%}
---
+++ BaselineB3Plot.m
@@ -0,0 +1,34 @@
+
+%plot things from each half cycle (can just copy and paste this section
+%once this has been run once)
+sz = [length(chargeCycles) 8];
+varNames = {'cycle_number','max_stress','start_stress','stress_change','capacity_start_mAhg','capacity_end_mAhg','charge_current','c_rate'};
+chronoArray = zeros(sz);
+for i = 1:length(chargeCycles)
+    hsi = find(chargeCycles{i}(:,6)<.011);
+    maxStrs = chargeCycles{i}(hsi(1),2);
+    startStrs = chargeCycles{i}(1,2);
+    strsChg = maxStrs-startStrs;
+    crate = round(abs(chargeCycles{i}(2,7)/C_rate),2);
+    chronoArray(i,:) = [i, maxStrs, startStrs, strsChg, chargeCycles{i}(hsi(1),9)/mass, chargeCycles{i}(end,9)/mass,chargeCycles{i}(2,7),crate];
+end
+
+chrono = array2table(chronoArray,'VariableNames',varNames)
+writetable(chrono,fullfile(projdir,'BaselineB3Chronology.csv'))
+
+
+figure(6)
+yyaxis left
+plot(chrono.cycle_number,chrono.start_stress,'-bo')
+hold on
+plot(chrono.cycle_number,chrono.max_stress,'-bs')
+plot(chrono.cycle_number,chrono.stress_change,'-b*')
+ylabel('Stress MPa')
+yyaxis right
+plot(chrono.cycle_number,chrono.capacity_start_mAhg,'-o')
+plot(chrono.cycle_number,chrono.capacity_end_mAhg,'-s')
+hold off
+legend('Stress at start of charge','Stress at end of charge','Stress Change','Capacity at hold start','Capacity at hold end')
+xlabel('Cycle Number')
+ylabel('Capacity mAh/g')
+title('Baseline B3 Cycles')
